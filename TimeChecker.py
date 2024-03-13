2e9b85f01f347b48a226c66b5f63644f163ccd40
ARnoLD <pavelmilosh31@gmail.com>
2024-03-13 18:57:34 +0300

    Working

--- TimeChecker.py
+++ TimeChecker.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
 import configparser
+import os
+import time
 from datetime import datetime
+
+def cls():
+	if os.name == "nt":
+		os.system("cls")
+	else:
+		os.system("clear")
 
 def set_time():
 	print("Set time. Ex: 14:50, 03:34. Or use empty string to set current time")
@@ -19,6 +28,31 @@ def start_activity(type_of):
 		"End_time": ""}
 	with open("settings.ini", "w") as configfile:
 		config.write(configfile)
+	quit(0)
+
+def statistic():
+	cls()
+	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	for element in range(0, current + 1):
+		if config[str(element)]["Start_time"][9] == "0":
+			current_month = months[int(config[str(element)]["Start_time"][10]) - 1]
+		else:
+			current_month = months[int(config[str(element)]["Start_time"][9:11]) - 1]
+		try:
+			if config[str(element + 1)]["Start_time"][9] == "0":
+				next_month = months[int(config[str(element + 1)]["Start_time"][10]) - 1]
+			else:
+				next_month = months[int(config[str(element + 1)]["Start_time"][9:11]) - 1]
+		except:
+			next_month = current_month
+		print(f'{config[str(element)]["Start_time"][6:8]} {current_month}: ')
+		print(f'    Type - {config[str(element)]["Type"]}')
+		print(f'    Start time - {config[str(element)]["Start_time"][0:5]}')
+		if config[str(element)]["Start_time"][9:11] == config[str(element)]["End_time"][9:11]:
+			print(f'    End time - {config[str(element)]["Start_time"][0:5]}')
+		else:
+			print(f'    End time - {config[str(element)]["Start_time"]}')
+	
 
 def main():
 	global current, config
@@ -26,6 +60,7 @@ def main():
 	config = configparser.ConfigParser()
 	config.read("settings.ini")
 	index = 0
+	cls()
 	try:
 		while True:
 			config[str(index)]["Type"]
@@ -35,12 +70,16 @@ def main():
 		pass
 	if config[str(current)]["End_time"] != "":
 		print("Choose the activity type: ")
+		print("[0] - STATISTICS")
 		index = 1
 		for element in types:
 			print(f"[{index}] - {element}")
 			index+=1
 		choose = int(input(">>> "))
-		start_activity(types[choose - 1])
+		if choose == 0:
+			statistic()
+		else:
+			start_activity(types[choose - 1])
 	else:
 		print(f'Your last activity "{config[str(current)]["Type"]} - {config[str(current)]["Start_time"]}" is not end. Do you want to add End time? [Y/N]')
 		choose = input(">>> ")
@@ -49,6 +88,8 @@ def main():
 			config[str(current)]["End_time"] = end_time
 			with open("settings.ini", "w") as configfile:
 				config.write(configfile)
+			time.sleep(2)
+			main()
 		else:
 			quit(0)
 main()
